3a0628d106027ac9ed5494c2e59c54f02733eb52
nimamasoumi <mraghahe@protonmail.com>
2023-10-12 20:00:36 -0400

    Graphical UI app

---
+++ Concepts/GUI/UserInterface.java
@@ -0,0 +1,13 @@
+package Concepts.GUI;
+
+import javax.swing.JFrame;
+
+class UserInterface
+{
+    public static void main(String[] args)
+    {
+        JFrame ui1 = new JFrame("GUI app");
+        ui1.setSize(313,313);
+        ui1.setVisible(true);
+    }   
+}
