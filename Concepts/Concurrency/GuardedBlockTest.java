74a91695faefe9868f3e55da11c5094cf0353218
nimamasoumi <mraghahe@protonmail.com>
2023-10-26 18:16:07 -0400

    Some minor changes

--- Concepts/Concurrency/GuardedBlockTest.java
+++ Concepts/Concurrency/GuardedBlockTest.java
@@ -1,11 +1,12 @@
 package Concepts.Concurrency;
 
 class GuardedBlockTest
-{
+{    
     public static void main(String[] args)
     {
         double[] rt = {0.0,0.0,0.0,0.0,0.0,0.0,0.0};
         DataClientServer dataRT = new DataClientServer(rt);
+        
         (new Thread(new ServerThread(dataRT))).start();
         (new Thread(new ClientThread(dataRT))).start();
     }
