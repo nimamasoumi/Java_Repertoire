6fc4a6c9aeccbc86eed6d16d3fb3af3263a49ac7
nimamasoumi <mraghahe@protonmail.com>
2024-02-12 22:37:31 -0500

    Adding a closed-form formula and the factorial function

--- Concepts/MathFormula/PowerSum.java
+++ Concepts/MathFormula/PowerSum.java
@@ -4,6 +4,10 @@
  * <code> long </code> type was used to store the results
  * Therefore, the summation is bounded by the supported range (64 bits)
  */
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class PowerSum extends CubicSum
 {
     /**
@@ -48,6 +52,48 @@ public class PowerSum extends CubicSum
         return accuNP(this.getNum(), this.getPower());        
     }
 
+    public double fact(int _n)
+    {
+        if(_n==0)
+        {
+            return 1.0;
+        }        
+        double mm =1, n=(double)_n;
+        while (n>0)
+        {
+            mm = n*mm;
+            n--;
+        }
+        return mm;
+    }
+
+    public long sumNP(long _num, long _pp)
+    {
+        var ss = new ArrayList<Long>();
+        ss.add(_num);
+        var np =(int) _pp;
+        int kk = 1;
+        double tSum=0, aSum = 0;
+        while (kk<np+1)
+        {
+            for(int ii=0; ii<=kk-1; ii++)
+            {
+                aSum += (this.fact(kk+1)/(this.fact(kk+1-ii)*this.fact(ii)))
+                        *(double)ss.get(ii);
+            }
+            tSum = (Math.pow((double)_num+1,(double)kk+1)-1-aSum)/((double)kk+1);
+            ss.add((long) tSum);
+            System.out.printf("\nIntermediate result: %d %d\n", kk, ss.get(kk));
+            kk++;
+        }
+
+        return ss.get(np).longValue();
+    }
+    public long sumNP()
+    {
+        return sumNP(this.getNum(), this.getPower());
+    }
+
     public static void main(String[] args)
     {
         if (args.length<2)
@@ -60,10 +106,18 @@ public class PowerSum extends CubicSum
         
         var n = Long.parseLong(args[0], 10);
         var p = Long.parseLong(args[1], 10);
+
+        if (p<1)
+        {
+            System.out.println("The power should be equal or bigger than 1.");
+            return;
+        }
+
         var pSum = new PowerSum(n, p);
         System.out.printf("\nThe number and power are: %d and %d\n"+
-        "The sum by accumulation is: %d\n", 
-        pSum.getNum(), pSum.getPower(), pSum.accuNP());   
+        "The sum by accumulation is: %d\n"+ 
+        "The sum using the closed form: %d\n",
+        pSum.getNum(), pSum.getPower(), pSum.accuNP(), pSum.sumNP());   
     }    
     
 }
