53a0f3ea06ff931169ce6310ca34064e7c407a8a
nimamasoumi <mraghahe@protonmail.com>
2024-02-22 16:27:21 -0500

    Calculation of PI in 1000 decimal points precision

--- Concepts/MathFormula/CalcPIConst.java
+++ Concepts/MathFormula/CalcPIConst.java
@@ -9,6 +9,7 @@
 package Concepts.MathFormula;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalcPIConst extends CalcEulerNum{
     public CalcPIConst(){};
@@ -33,28 +34,31 @@ public class CalcPIConst extends CalcEulerNum{
     public BigDecimal factbd(int _n){
         if(_n==0)
         {
-            return new BigDecimal("1.0");
+            return BigDecimal.valueOf(1.0);
         }        
         var n=(double)_n;
-        var mm = new BigDecimal("1.0");
+
+        // similar to new BigDecimal("1.0");
+        var mm = BigDecimal.valueOf(1.0);
         mm.setScale(1000);
         while (n>0)
         {
-            mm = mm.multiply(new BigDecimal(n));
+            mm = mm.multiply(BigDecimal.valueOf(n));
             n=n-1;
         }
         return mm;
     }
 
+    // In this method, double and Double are not used
     public BigDecimal calcPIbd(){
         int n = this.getPrec();        
-        var sss=new BigDecimal("0.0");
-        var pol=new BigDecimal("0.5");
-        var num=new BigDecimal(0.0);
-        var dnum=new BigDecimal(0.0);
-        var dnum2=new BigDecimal(0.0);
-        var dnum3=new BigDecimal(0.0);
-        var dnum4=new BigDecimal(0.0);
+        var sss=BigDecimal.valueOf(0.0);
+        var pol=BigDecimal.valueOf(0.5);
+        var num=BigDecimal.valueOf(0.0);
+        var dnum=BigDecimal.valueOf(0.0);
+        var dnum2=BigDecimal.valueOf(0.0);
+        var dnum3=BigDecimal.valueOf(0.0);
+        var dnum4=BigDecimal.valueOf(0.0);
         sss.setScale(1000);
         while(n>-1)
         {
@@ -67,7 +71,7 @@ public class CalcPIConst extends CalcEulerNum{
             dnum3=dnum3.pow(n);
             dnum4 = BigDecimal.valueOf(1+(2*n));
             dnum = dnum3.multiply(dnum4.multiply(dnum2.pow(2)));            
-            sss = sss.add(num.divide(dnum));
+            sss = sss.add(num.divide(dnum, 1000, RoundingMode.DOWN));
             n--;
             pol= BigDecimal.valueOf(0.5);
         }
@@ -76,14 +80,22 @@ public class CalcPIConst extends CalcEulerNum{
 
     public static void main(String[] args)
     {
-        int prec = 19;
+        int prec = 1000;
         var calcPI = new CalcPIConst(prec);
+        var piStr = calcPI.calcPIbd().toString();
+        var piStrD = piStr.substring(2);
+        var dP = piStrD.length();
+        while(dP>10){
+            piStrD = new StringBuilder(piStrD).insert(dP-10,',').toString();
+            dP=dP-10;
+        }        
+        //System.out.println(piStrD);
 
         System.out.printf("\nThe PI value using %d taylor series terms is:\n"+
         "%.19f\n"+"The PI constant according to Java Math package is:\n"+"%.19f\n"+
-        "Using the BigDecimal to accumulate the value:\n"+"%s\n",
-        calcPI.getPrec(),calcPI.calcPI().doubleValue(),Math.PI, calcPI.calcPIbd().toString());
+        "Using the BigDecimal to accumulate the value:\n"+"3.%s\n",
+        calcPI.getPrec(),calcPI.calcPI().doubleValue(),Math.PI, piStrD);
 
-        System.out.println(calcPI.factbd(prec));
+        //System.out.println(calcPI.factbd(prec));
     }
 }
